da2e50acc5a6b94c71325cdd747eb03ed3eb127b
Stepan Hrbek <stepan.git@lightsprint.com>
2023-08-20 18:44:49 +0200

    Add sources for html files.

---
+++ src/build.bat
@@ -0,0 +1,7 @@
+@echo off
+call build_1 index
+call build_1 features
+call build_1 demo
+call build_1 evaluation
+call build_1 charity
+call build_1 contact
---
+++ src/build_1.bat
@@ -0,0 +1,7 @@
+copy header ..\docs\%1.html
+type x_%1 >> ..\docs\%1.html
+type menu >> ..\docs\%1.html
+type x_%1.html >> ..\docs\%1.html
+type footer >> ..\docs\%1.html
+
+rem copy header + x_%1 + menu + x_%1.html + footer ..\docs\%1.html
